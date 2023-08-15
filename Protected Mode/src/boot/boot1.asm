1395789b85be69da7412f80887232365c898edd4
Anish Patil <anishpatil0307@gmail.com>
2023-08-15 07:17:58 +0530

    Enabling A20 Line

--- Protected Mode/src/boot/boot1.asm
+++ Protected Mode/src/boot/boot1.asm
@@ -20,16 +20,15 @@ step2:
     mov ax,0x00
     mov ds,ax
     mov es,ax
-    mov ax,0x00
-    mov sp,0x7c00
     mov ss,ax
+    mov sp,0x7c00
     sti ;Enables Interrupts
 
 .load_protected:
     cli
     lgdt[gdt_descriptor]
     mov eax,cr0
-    or eax,0x1
+    or eax,0x1 
     mov cr0,eax
     jmp CODE_SEG:load32
 
@@ -39,9 +38,9 @@ gdt_null:
     dd 0x0
     dd 0x0
 ;offset 0x08
-gdt_code:       ;CS should point to this
+gdt_code:     ;CS should point to this
     dw 0xffff ; Segment limit first 0-15 bits
-    dw 0      ; Base first 0-15 bits
+    dw 0      ; Base first 0-15 bits 
     db 0      ; Base 16-23
     db 0x9a   ; Access byte
     db 11001111b; high 4 bit flags and low 4 bit flags
@@ -64,14 +63,21 @@ gdt_descriptor:
 
 [BITS 32]
 load32:
+;Setting up all register in protected mode
     mov ax,DATA_SEG
     mov ds,ax
     mov es,ax
     mov fs,ax
     mov gs,ax
     mov ss,ax
-    mov ebp,0x00200000
+    mov ebp,0x00200000    ;ebp- Base pointer
     mov esp,ebp
+
+    ;Enabling A20 Line to access above 1MB limit
+    in al,0x92
+    or al,2
+    out 0x92,al
+
     jmp $
 
 times 510 -($ - $$) db 0
